1c6ff7f5361dafb351b02fa8212866e3bc303e8e
Omar <Omar.salemps@gmail.com>
2022-12-29 17:06:51 +0200

    add a new model

--- server/models/frontEnd.model.js
+++ server/models/frontEnd.model.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 const frontEndSchema = new mongoose.Schema({
     html: {
         type: Array
@@ -9,27 +8,5 @@ const frontEndSchema = new mongoose.Schema({
     height: {type:Number},
     color: {type:String},
     BGColor: {type:String},
-
-    firstName: {
-    type: String,
-    required: [true, "First name is required"]
-    },
-    lastName: {
-    type: String,
-    required: [true, "Last name is required"]
-    },
-    email: {
-    type: String,
-    required: [true, "Email is required"],
-    validate: {
-        validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
-        message: "Please enter a valid email"
-    }
-    },
-    password: {
-    type: String,
-    required: [true, "Password is required"],
-    minlength: [8, "Password must be 8 characters or longer"]
-    }
 }, {timestamps: true});
 module.exports.FrontEnd = mongoose.model('frontEnd', frontEndSchema)
