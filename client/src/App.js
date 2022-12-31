d7cb90b5d7376add57a332e922ca816b51a1da93
Karam Taha <karamtaha15@gmail.com>
2022-12-31 14:24:02 +0200

    added navbar and some view components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import DisplayContainer from './views/DisplayContainer/DisplayContainer';
+import AboutUs from './views/AboutUs/AboutUs';
+import Dashboard from './views/Dashboard/Dashboard';
+import MyProjects from './views/MyProjects/MyProjects';
+import Profile from './views/Profile/Profile';
 import {Routes, Route} from 'react-router-dom';
     
 function App() {
@@ -8,6 +12,10 @@ function App() {
         <>
             <Routes>
                 <Route element={<DisplayContainer/>} path='/'/>
+                <Route element={<Dashboard/>} path='/home'/>
+                <Route element={<Profile/>} path='/profile'/>
+                <Route element={<MyProjects/>} path='/projects'/>
+                <Route element={<AboutUs/>} path='/about'/>
             </Routes>
         </>
     );
